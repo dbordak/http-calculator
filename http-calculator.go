c6e199baa245a04ca5e787c429c63d293bfdc677
Daniel Bordak <dbordak@fastmail.fm>
2018-02-08 16:32:02 -0500

    Handle type conversion on arguments

--- http-calculator.go
+++ http-calculator.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"strconv"
 )
+
+func argHandler(q url.Values) (int, int, error) {
+	xs := q.Get("x")
+	ys := q.Get("y")
+	if xs == "" || ys == "" {
+		return 0, 0, errors.New("Argument Missing")
+	}
+
+	x, err := strconv.Atoi(xs)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	y, err := strconv.Atoi(ys)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return x, y, nil
+}
 
 func main() {
 	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
-		a := r.URL.Query().Get("a")
-		b := r.URL.Query().Get("b")
-		if a == "" || b == "" {
-			return
+		x, y, err := argHandler(r.URL.Query())
+		if err != nil {
+			// TODO
 		}
-		fmt.Fprintf(w, "a=%q, b=%q", a, b)
+
+		fmt.Fprintf(w, "x+y=%d", x+y)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
